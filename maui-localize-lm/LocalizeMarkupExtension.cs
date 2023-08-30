7cb492b6b991eb408da849a51736a61ea1ef874b
Stephen Quan <squan@esri.com>
2023-08-30 16:49:12 +1000

    Simplify XAML with LocalizeMarkupExtension that wraps Binding+LocalizationManager.Current

---
+++ maui-localize-lm/LocalizeMarkupExtension.cs
@@ -0,0 +1,17 @@
+﻿namespace maui_localize_lm;
+
+[ContentProperty(nameof(Path))]
+public class LocalizeExtension : IMarkupExtension<BindingBase>
+{
+    public string Path { get; set; } = ".";
+    public BindingMode Mode { get; set; } = BindingMode.OneWay;
+    public IValueConverter Converter { get; set; } = null;
+    public string ConverterParameter { get; set; } = null;
+    public string StringFormat { get; set; } = null;
+
+    public object ProvideValue(IServiceProvider serviceProvider)
+        => (this as IMarkupExtension<BindingBase>).ProvideValue(serviceProvider);
+
+    BindingBase IMarkupExtension<BindingBase>.ProvideValue(IServiceProvider serviceProvider)
+        => new Binding(Path, Mode, Converter, ConverterParameter, StringFormat, LocalizationManager.Current);
+}
