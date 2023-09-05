c584e7bd8aa1d3a5a3df3a8dddf6460b0e3f0aa5
Stephen Quan <squan@esri.com>
2023-09-05 21:25:39 +1000

    Improve LM language selector

---
+++ maui-localize-lm/BoolToColorConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace maui_localize_lm;
+
+public class BoolToColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (bool)value
+            ? Colors.LightSteelBlue
+            : Colors.Transparent;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ maui-localize-lm/LanguageInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+
+namespace maui_localize_lm;
+
+public class LanguageInfo : CultureInfo, INotifyPropertyChanged
+{
+    public LanguageInfo(string language) : base(language)
+        => LocalizationManager.CultureChanged += OnCultureChanged;
+
+    ~LanguageInfo()
+        => LocalizationManager.CultureChanged -= OnCultureChanged;
+
+    private void OnCultureChanged(object sender, EventArgs e)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsCurrent)));
+
+    public bool IsCurrent
+        => Name == CultureInfo.CurrentUICulture.Name;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+}
