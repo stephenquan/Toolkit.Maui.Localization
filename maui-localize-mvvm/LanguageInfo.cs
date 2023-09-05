d99483b44982f8de67d80957cb5348c2f72e9e54
Stephen Quan <squan@esri.com>
2023-09-06 07:55:25 +1000

    Update Language Selector in MVVM sample

---
+++ maui-localize-mvvm/LanguageInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+
+namespace maui_localize_mvvm;
+
+public class LanguageInfo : CultureInfo, INotifyPropertyChanged
+{
+    public static EventHandler CultureChanged;
+
+    public static CultureInfo Culture
+    {
+        get => CultureInfo.CurrentUICulture;
+        set
+        {
+            if (value.Name == CultureInfo.CurrentUICulture.Name) return;
+            CultureInfo.CurrentCulture = CultureInfo.CurrentUICulture = value;
+            CultureChanged?.Invoke(null, EventArgs.Empty);
+        }
+    }
+
+    public LanguageInfo(string language) : base(language)
+        => CultureChanged += OnCultureChanged;
+
+    ~LanguageInfo()
+        => CultureChanged -= OnCultureChanged;
+
+    private void OnCultureChanged(object sender, EventArgs e)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsCurrent)));
+
+    public bool IsCurrent
+        => Name == CultureInfo.CurrentUICulture.Name;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+}
